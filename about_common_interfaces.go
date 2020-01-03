9820e03fcff1bee297b974979bbcd2bde0e04d11
Charlie Depman <cdepman@gmail.com>
2020-01-03 12:56:52 -0800

    finish about common interfaces

--- about_common_interfaces.go
+++ about_common_interfaces.go
@@ -1,6 +1,9 @@
 package go_koans
 
-import "bytes"
+import (
+	"bytes"
+	"fmt"
+)
 
 func aboutCommonInterfaces() {
 	{
@@ -8,7 +11,7 @@ func aboutCommonInterfaces() {
 		in.WriteString("hello world")
 
 		out := new(bytes.Buffer)
-
+		out.ReadFrom(in)
 		/*
 		   Your code goes here.
 		   Hint, use these resources:
@@ -26,7 +29,11 @@ func aboutCommonInterfaces() {
 		in.WriteString("hello world")
 
 		out := new(bytes.Buffer)
-
+		portion, err := in.ReadBytes('o')
+		if err != nil {
+		}
+		out.Write(portion)
+		fmt.Printf("%s", out.String())
 		assert(out.String() == "hello") // duplicate only a portion of the io.Reader
 	}
 }
